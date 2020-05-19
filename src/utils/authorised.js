71f18a3923780a17d57c9891650dcdc45bd2052d
Ivo Evans <ievans.storrie@gmail.com>
2020-05-19 17:23:11 +0100

    Add authorised function, totally untested as of yet - will wait for backend or write fake fetch. yet to write validateExp.
    Co-authored-by: Vatskan <vatskan@gmail.com>

--- src/utils/authorised.js
+++ src/utils/authorised.js
@@ -1,5 +1,26 @@
-function authorised() {
+import validateExp from './validateExp';
+
+async function authorised() {
+	const authString = localStorage.getItem('jwt');
+	if (!authString) {
+		return false;
+	}
+	const auth = JSON.parse(authString);
+	if (validateExp(auth.exp)) {
+		return fetch(`/server/verify/route/${auth.jwt}`).then(
+			(res) => res.status === '200',
+		);
+	}
 	return false;
+	// convert auth string to object
+	// return await fetch request to server, if res.status is 200 return true, else return false
 }
 
 module.exports = authorised;
+
+// const sampleIncomingLogin = {
+// 	"user_id": 500,
+// 	"user_name": "Ivo",
+// 	"expires": "11589932799", // this is midnight tonight
+// 	"jwt": "123456789iasdof904rpg;nas[0u23rlnSDFAKSDF"
+// }
---
+++ src/utils/validateExp.js
@@ -0,0 +1,3 @@
+export default function validateExp(exp) {
+	Number(exp);
+}
