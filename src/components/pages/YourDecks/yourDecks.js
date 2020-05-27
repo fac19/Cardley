219edde079adedf678dab1c7cdfceba94157e871
Ivo Evans <ievans.storrie@gmail.com>
2020-05-27 11:06:48 +0100

    Add your decks page with some basic buttons
    Co-authored-by: Roger-Heathcote <github@technicalbloke.com>

--- src/components/pages/YourDecks/yourDecks.js
+++ src/components/pages/YourDecks/yourDecks.js
@@ -1,18 +1,77 @@
 import React from 'react';
-import ViewDecks from '../../viewDecks/viewDecks';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
+import ViewDecksLinks from './ViewDecksLinks';
 import useYourDecks from '../../../hooks/useYourDecks';
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+			width: '25ch',
+		},
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
+}));
+
 export default function MyDecks() {
+	const classes = useStyles();
 	const [decks, setDecks] = React.useState(null);
 	const [selectedDecks, setSelectedDecks] = React.useState({});
 	useYourDecks({ setDecks, setSelectedDecks });
+	const history = useHistory();
 
 	return (
-		<ViewDecks
-			selectedDecks={selectedDecks}
-			setSelectedDecks={setSelectedDecks}
-			decks={decks}
-			setDecks={setDecks}
-		/>
+		<>
+			<ViewDecksLinks
+				selectedDecks={selectedDecks}
+				setSelectedDecks={setSelectedDecks}
+				decks={decks}
+				setDecks={setDecks}
+			/>
+
+			<Button
+				type="submit"
+				fullWidth
+				variant="contained"
+				color="primary"
+				className={classes.submit}
+				onClick={() => {
+					history.push('/create-deck');
+				}}
+			>
+				Create Deck
+			</Button>
+
+			<Button
+				type="submit"
+				fullWidth
+				variant="contained"
+				color="primary"
+				className={classes.submit}
+				onClick={() => {
+					history.push('/public-decks');
+				}}
+			>
+				Public Decks
+			</Button>
+		</>
 	);
 }
